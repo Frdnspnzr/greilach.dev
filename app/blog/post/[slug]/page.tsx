aeb605708d4de049f0dd09e4787e3d01f147c870
Pascal Greilach <pascal@greilach.dev>
2024-01-23 15:15:17 +0100

    Add basic layout for navigation

--- app/blog/post/[slug]/page.tsx
+++ app/blog/post/[slug]/page.tsx
@@ -1,28 +1,39 @@
+import BackToFrontPage from "@/components/BackToFrontPage";
+import Navigation from "@/components/Navigation/Navigation";
 import { getAllPosts, getPost } from "@/lib/fetchers";
 import { Metadata } from "next";
 import { MDXRemote } from "next-mdx-remote/rsc";
+import Link from "next/link";
 
 type Props = { params: { slug: string } };
 
 export default async function Page({ params }: Props) {
   const post = getPost(params.slug);
   return (
-    <div className="with-sidebar">
-      <div className="sidebar">
-        <div>
-          <h2>Date</h2>
-          {post.date.toLocaleDateString()}
+    <>
+      <nav>
+        <BackToFrontPage />
+        <Link href="/blog">
+          <Navigation depth="landingpage">Back to all posts</Navigation>
+        </Link>
+      </nav>
+      <div className="with-sidebar">
+        <div className="sidebar">
+          <div>
+            <h2>Date</h2>
+            {post.date.toLocaleDateString()}
+          </div>
+          <div>
+            <h2>Author</h2>
+            <p>Pascal Greilach</p>
+          </div>
         </div>
-        <div>
-          <h2>Author</h2>
-          <p>Pascal Greilach</p>
+        <div className="text-container">
+          <h1>{post.title}</h1>
+          <MDXRemote source={post.content} />
         </div>
       </div>
-      <div className="text-container">
-        <h1>{post.title}</h1>
-        <MDXRemote source={post.content} />
-      </div>
-    </div>
+    </>
   );
 }
 
